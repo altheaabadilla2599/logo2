c750982019ba0f35ad44308d5500020f8ed84e23
altheaabadilla2599 <70379536+altheaabadilla2599@users.noreply.github.com>
2020-09-09 10:28:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE html>
+<html>
+
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>Logo2</title>
+
+
+
+
+
+
+
+<div class="scoop1"></div>
+<div class="scoop2"></div>
+<div class="scoop3"></div>
+<div class="cone">
+<div class="topping1"></div>
+     <div class="topping11" ></div>
+	 <div class="topping111" ></div>
+	 <div class="topping1111" ></div>
+	 <div class="topping0" ></div>
+	  <div class="topping00" ></div>
+	   <div class="topping01" ></div>
+	    <div class="topping02" ></div>
+		 <div class="topping03" ></div>
+		
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</html>
